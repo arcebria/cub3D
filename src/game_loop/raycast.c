a42aa45ff711c13ce6e059e3157e38893aef4562
Arnau Cebrian I Ortega <arcebria@car14s1.42barcelona.com>
2025-05-27 19:16:26 +0200

    mal lo de las puertas

--- src/game_loop/raycast.c
+++ src/game_loop/raycast.c
@@ -1,16 +1,98 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   raycast_utils1.c                                   :+:      :+:    :+:   */
+/*   raycast.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: arcebria <arcebria@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 18:18:14 by arcebria          #+#    #+#             */
-/*   Updated: 2025/05/23 19:06:47 by arcebria         ###   ########.fr       */
+/*   Updated: 2025/05/27 19:14:24 by arcebria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/cub3D.h"
+
+void	render_door_texture(t_game *game, t_raycast *values, int x)
+{
+	int			y;
+	int			screen_to_tex;
+	uint8_t		*pixel;
+	uint32_t	color;
+
+	y = values->draw_start;
+	while (y < values->draw_end)
+	{
+		screen_to_tex = y * 256 - SCREEN_HEIGHT * 128
+			+ values->wall_height * 128;
+		values->tex_y = ((screen_to_tex * values->tex_height)
+				/ values->wall_height) / 256;
+		pixel = &values->wall_texture->pixels[4 * (values->tex_width
+				* values->tex_y + values->tex_x)];
+		color = (pixel[0] << 24) | (pixel[1] << 16)
+			| (pixel[2] << 8) | pixel[3];
+		mlx_put_pixel(game->img, x, y, color);
+		y++;
+	}
+}
+
+void	calculate_texture_door(t_game *game, t_raycast *values, int x)
+{
+	if (values->side == 0)
+		values->hit_in_wall = game->pos_y + values->wall_dist
+			* values->ray_dir_y;
+	else if (values->side == 1)
+		values->hit_in_wall = game->pos_x + values->wall_dist
+			* values->ray_dir_x;
+	values->hit_in_wall -= floor(values->hit_in_wall);
+	values->tex_height = values->wall_texture->height;
+	values->tex_width = values->wall_texture->width;
+	values->tex_x = (int)(values->hit_in_wall * (double)values->tex_width);
+	if (values->side == 0 && values->ray_dir_x < 0)
+		values->tex_x = values->tex_width - values->tex_x - 1;
+	if (values->side == 1 && values->ray_dir_y > 0)
+		values->tex_x = values->tex_width - values->tex_x - 1;
+	render_door_texture(game, values, x);
+}
+
+void	calculate_door_height(t_game *game, t_raycast *values, int x)
+{
+	if (values->side == 0)
+		values->wall_dist = values->side_dist_x - values->delta_dist_x;
+	else if (values->side == 1)
+		values->wall_dist = values->side_dist_y - values->delta_dist_y;
+	values->wall_height = (int)(SCREEN_HEIGHT / values->wall_dist);
+	values->draw_start = SCREEN_HEIGHT / 2 - values->wall_height / 2;
+	if (values->draw_start < 0)
+		values->draw_start = 0;
+	values->draw_end = SCREEN_HEIGHT / 2 + values->wall_height / 2;
+	if (values->draw_end >= SCREEN_HEIGHT)
+		values->draw_end = SCREEN_HEIGHT - 1;
+	calculate_texture_door(game, values, x);
+}
+
+void	calculate_door_hit(t_game *game, t_raycast *values, int x)
+{
+	values->hit = 0;
+	while (values->hit == 0)
+	{
+		if (values->side_dist_x < values->side_dist_y)
+		{
+			values->side_dist_x += values->delta_dist_x;
+			values->map_x += values->step_x;
+			values->side = 0;
+		}
+		else
+		{
+			values->side_dist_y += values->delta_dist_y;
+			values->map_y += values->step_y;
+			values->side = 1;
+		}
+		if (game->map->map[values->map_y][values->map_x] == '2')
+			values->hit = 1;
+	}
+	values->wall_texture = mlx_load_png("texs/door.png");
+	calculate_door_height(game, values, x);
+}
 
 void	raycasting(t_game *game)
 {
@@ -25,6 +107,7 @@ void	raycasting(t_game *game)
 		calculate_delta_dist(&values);
 		calculate_side_dist(game, &values);
 		calculate_hits(game, &values);
+		calculate_door_hit(game, &values, x);
 		assign_textures(game, &values);
 		calculate_wall_height(&values);
 		calculate_texture_x(game, &values);
